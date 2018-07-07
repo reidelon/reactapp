f839c6e37fa23360fde3310ef4633792fe3799ae
Reidel Lazaro Rodriguez Torres <reidelon@gmail.com>
2018-07-07 13:52:15 -0400

    adding simple routing and an "about page"

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
 $ = jQuery = require('jquery');
 var React = require('react');
-var Home = require('./components/homePage')
-React.render(<Home/>, document.getElementById('app'));
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+
+(function (win) {
+ "use strict";
+var App = React.createClass({
+    render: function () {
+        var Child;
+
+        switch (this.props.route){
+            case 'about': Child = About; break;
+            default: Child = Home;
+        }
+        return (
+          <div>
+              <Child/>
+          </div>
+        );
+    }
+});
+
+function render() {
+    var route = win.location.hash.substr(1);
+    React.render(<App route={route}/>, document.getElementById('app'));
+}
+win.addEventListener('hashchange', render);
+render();
+}(window));
