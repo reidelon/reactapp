e1ef3ff2ed9c2e14fcc647b1e80583ceda155b59
Reidel Lazaro Rodriguez Torres <reidelon@gmail.com>
2018-07-22 17:04:51 -0400

    the form has validation, added redirect to list authors after add an author

--- src/components/authors/authorsList.js
+++ src/components/authors/authorsList.js
@@ -1,17 +1,19 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 var AuthorApi = require('../../api/authorApi');
 
 var AuthorList = React.createClass({
-    propTypes:{
+    propTypes: {
         authors: React.PropTypes.array.isRequired
     },
     render: function () {
         var createAuthorRow = function (author) {
             return (
                 <tr key = {author.id}>
-                    <td><a href={"/#authors/" + author.id}>{author.id}</a></td>
+                    <td><Link to="manageAuthor" params={{id: author.id}}>{author.id}</Link></td>
                     <td>{author.firstName}{author.lastName}</td>
                 </tr>
             );
--- src/main.js
+++ src/main.js
@@ -1,34 +1,9 @@
-$ = jQuery = require('jquery');
+"use strict";
 var React = require('react');
-var Home = require('./components/homePage');
-var About = require('./components/about/aboutPage');
-var Header = require('./components/common/header');
-var Authors = require('./components/authors/authorsPage');
+var Router = require('react-router');
+var routers = require('./routes');
 
-(function (win) {
- "use strict";
-var App = React.createClass({
-    render: function () {
-        var Child;
-
-        switch (this.props.route){
-            case 'about': Child = About; break;
-            case 'authors': Child = Authors; break;
-            default: Child = Home;
-        }
-        return (
-          <div>
-              <Header/>
-              <Child/>
-          </div>
-        );
+Router.run(routers, function (Handler) {
+        React.render(<Handler/>, document.getElementById('app'));
     }
-});
-
-function render() {
-    var route = win.location.hash.substr(1);
-    React.render(<App route={route}/>, document.getElementById('app'));
-}
-win.addEventListener('hashchange', render);
-render();
-}(window));
+);
