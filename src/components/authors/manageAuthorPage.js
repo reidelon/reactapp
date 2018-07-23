6e4a904ea54635e3c999ddbbf431df9f86afb1a7
Reidel Lazaro Rodriguez Torres <reidelon@gmail.com>
2018-07-23 13:28:28 -0400

    adding notification when an author is added

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -4,6 +4,7 @@ var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
 
 var ManageAuthorpage = React.createClass({
     mixins: [
@@ -59,7 +60,7 @@ var ManageAuthorpage = React.createClass({
         }
         AuthorApi.saveAuthor(this.state.author);
         this.setState({dirty: false});
-        // toastr.success('Author saved.');
+        toastr.success('Author saved.');
         this.transitionTo('authors');
     },
     render: function () {
